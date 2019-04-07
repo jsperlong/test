b38f077b0753f223886f508783dd878ca10bd0e1
longht <1280200617@qq.com>
2019-04-07 15:21:43 +0800

    vue test

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,121 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>Essential Links</h2>
+    <ul>
+      <li>
+        <a href="https://vuejs.org" target="_blank">Core Docs</a>
+      </li>
+      <li>
+        <a href="https://forum.vuejs.org" target="_blank">Forum</a>
+      </li>
+      <li>
+        <a href="https://chat.vuejs.org" target="_blank">Community Chat</a>
+      </li>
+      <li>
+        <a href="https://twitter.com/vuejs" target="_blank">Twitter</a>
+      </li>
+      <br>
+      <li>
+        <a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a>
+      </li>
+    </ul>
+    <h2>Ecosystem</h2>
+    <ul>
+      <li>
+        <a href="http://router.vuejs.org/" target="_blank">vue-router</a>
+      </li>
+      <li>
+        <a href="http://vuex.vuejs.org/" target="_blank">vuex</a>
+      </li>
+      <li>
+        <a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a>
+      </li>
+      <li>
+        <a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  data() {
+    return {
+      msg: "Welcome to Your Vue.js App"
+    };
+  },
+  methods: {
+    test() {
+      let a = {bb:'1'};
+      let b = a;
+      a.data = '200';
+      console.log(a,b)
+    },
+    bubbleSort(arr) {
+      console.time("改进后冒泡排序耗时");
+      var len = arr.length;
+      for (var i = 0; i < len; i++) {
+        for (var j = 0; j < len - 1 - i; j++) {
+          if (arr[j] > arr[j + 1]) {
+            //相邻元素两两对比
+            var temp = arr[j + 1]; //元素交换
+            arr[j + 1] = arr[j];
+            arr[j] = temp;
+          }
+        }
+      }
+      console.timeEnd("改进后冒泡排序耗时");
+      return arr;
+    },
+    bubbleSort2(arr) {
+      console.time("改进后冒泡排序耗时");
+      var i = arr.length - 1; //初始时,最后位置保持不变
+      while (i > 0) {
+        var pos = 0; //每趟开始时,无记录交换
+        for (var j = 0; j < i; j++) {
+          if (arr[j] > arr[j + 1]) {
+            pos = j; //记录交换的位置
+            var tmp = arr[j];
+            arr[j] = arr[j + 1];
+            arr[j + 1] = tmp;
+          }
+        }
+        i = pos; //为下一趟排序作准备
+        console.log(i)
+      }
+      console.timeEnd("改进后冒泡排序耗时");
+      return arr;
+    }
+  },
+  mounted() {
+    this.test()
+    // console.log(
+    //   this.bubbleSort([3, 44, 38, 5, 47, 15, 36, 26, 27, 2, 46, 4, 19, 50, 48])
+    // );
+    console.log(
+      this.bubbleSort2([3, 44, 38, 5, 47, 15, 36, 26, 27, 2, 46, 4, 19, 50, 48])
+    );
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
